f2d1fdd36a10c62a60fa2ae47bca4e1c1c6d26a4
knowankit <44323532+knowankit@users.noreply.github.com>
2023-11-20 22:00:26 +0530

    feat: add image generation skeleton

---
+++ src/pages/api/generate-image.ts
@@ -0,0 +1,52 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI,
+  organization: process.env.ORGANISATION_ID
+});
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    // Process a POST request
+    const text = req.body.searchText
+    if(!text) {
+      res.status(401).json({ type: 'error', message: 'Please add a text'})
+    }
+    if (text.length < 3) {
+      res.status(401).json({ type: 'error', message: 'Too short text'})
+    }
+    const image = await getImage(text)
+    res.status(200).json(image)
+  } else {
+    // Handle any other HTTP method
+  }
+}
+
+const getImage = async (text: string) => {
+  const image = await openai.images.generate({ model: "dall-e-2", prompt: text, size: '256x256' });
+
+  // console.log("image", image);
+  // console.log("image data", image.data);
+
+  return image.data
+
+  return [
+    {
+      revised_prompt: "A delightful baby with a blush of playfulness in his eye, exercising caution as he interacts with a lion. Keep the atmosphere light and innocent, focusing on the gentle lion as an exceedingly tame creature. The lion should appear beautiful and majestic, yet utterly harmless, allowing the tiny tot, of Caucasian descent, to frolic around without any fear. The setting is a sunny outdoor landscape, with the sun's delicate rays bathing the scene in a soft, gentle glow. Please do not create a dangerous or fearful scenario.",
+      url: 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-OozkG59BgSO1t10iPg6gosOK/user-Vb58CjaCp6ngScKzWmKzaojq/img-u3V4MfsPtySUNMfipxb1xL8S.png?st=2023-11-20T14%3A35%3A55Z&se=2023-11-20T16%3A35%3A55Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-11-19T20%3A49%3A56Z&ske=2023-11-20T20%3A49%3A56Z&sks=b&skv=2021-08-06&sig=U0BEjmM7Pv0oG%2BSkEBKhFio6eCiRz61fQI0MaMD8WdY%3D'
+    }
+  ]
+}
+
+export default handler
+
+/**
+ * [
+  {
+    revised_prompt: "A delightful baby with a blush of playfulness in his eye, exercising caution as he interacts with a lion. Keep the atmosphere light and innocent, focusing on the gentle lion as an exceedingly tame creature. The lion should appear beautiful and majestic, yet utterly harmless, allowing the tiny tot, of Caucasian descent, to frolic around without any fear. The setting is a sunny outdoor landscape, with the sun's delicate rays bathing the scene in a soft, gentle glow. Please do not create a dangerous or fearful scenario.",
+    url: 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-OozkG59BgSO1t10iPg6gosOK/user-Vb58CjaCp6ngScKzWmKzaojq/img-u3V4MfsPtySUNMfipxb1xL8S.png?st=2023-11-20T14%3A35%3A55Z&se=2023-11-20T16%3A35%3A55Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-11-19T20%3A49%3A56Z&ske=2023-11-20T20%3A49%3A56Z&sks=b&skv=2021-08-06&sig=U0BEjmM7Pv0oG%2BSkEBKhFio6eCiRz61fQI0MaMD8WdY%3D'
+  }
+]
+ */
--- src/pages/image-generation.tsx
+++ src/pages/image-generation.tsx
@@ -1,5 +1,77 @@
+import React, { useState } from "react";
+import { TextField, Button, Box, Typography } from "@mui/material";
+import Image from "next/image";
+
 const ImageGeneration = () => {
-  return "Image Generation";
+  const [searchText, setSearchText] = useState("");
+  const [imageData, setImageData] = useState<any>({});
+
+  const handleSearch = async () => {
+    // Add your logic here for generating images based on the search text
+    console.log(`Generating images for: ${searchText}`);
+
+    const response = await fetch("http://localhost:3000/api/generate-image", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ searchText })
+    });
+
+    const result = await response.json();
+
+    if (Array.isArray(result)) {
+      setImageData(result[0]);
+    }
+  };
+
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+      height="100vh"
+      width="100vw"
+    >
+      <Box>
+        <TextField
+          label="Search"
+          autoComplete="off"
+          size="small"
+          variant="outlined"
+          value={searchText}
+          onChange={e => setSearchText(e.target.value)}
+          sx={{ marginRight: 1 }}
+        />
+        <Button
+          variant="contained"
+          onClick={handleSearch}
+          disabled={searchText.length < 3}
+          sx={{ textTransform: "none" }}
+        >
+          Generate Image
+        </Button>
+      </Box>
+
+      {imageData.url && (
+        <>
+          <Box mt={4}>
+            <Box
+              component="img"
+              src={imageData.url}
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </Box>
+          <Box width="36rem">
+            <Typography>{imageData.revised_prompt}</Typography>
+          </Box>
+        </>
+      )}
+    </Box>
+  );
 };
 
 export default ImageGeneration;
