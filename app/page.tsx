7d00646a7ac12292f2d3117c273013e2954f4b5e
knowankit <44323532+knowankit@users.noreply.github.com>
2023-11-20 16:00:21 +0530

    init skeleton

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+import Hello from "@/src/components/hello";
+
+export default function Home() {
+  return <Hello />;
+}
---
+++ src/components/hello.tsx
@@ -0,0 +1,5 @@
+const Hello = () => {
+  return <p>Open AI demos</p>;
+};
+
+export default Hello;
